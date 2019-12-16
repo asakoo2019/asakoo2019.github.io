c2d9706d663f506b62a90d0167e2b5a5afedb4ee
Hovo91 <hovomnacakanyan91@gmail.com>
2019-12-16 11:08:27 +0400

    sign in

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 
-import Header from '../components/header';
-import NavBar from '../components/header/nav-bar'
-import Footer from '../components/footer';
+import SignIn from '../components/sign-in';
 
 
 const styles = {
@@ -14,9 +12,7 @@ function App(props) {
   const { classes } = props;
   return (
     <div className={classes.main}>
-      <Header />
-      <NavBar />
-      <Footer />
+      <SignIn />
     </div>
   );
 };
