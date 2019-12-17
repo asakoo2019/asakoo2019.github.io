591cd14ddd6ef1fdb67d430245334628b14a774a
Hovo91 <hovomnacakanyan91@gmail.com>
2019-12-17 18:54:40 +0400

    modal

--- src/components/user-profile/user-profile.js
+++ src/components/user-profile/user-profile.js
@@ -4,6 +4,7 @@ import DropzoneDialogBlock from '../image-uploader-block';
 import userImage from './1.jpg'
 import { Grid } from '@material-ui/core';
 import './user-profile.css';
+import FormDialog from '../modal';
 
 // const user = firestore.collection('users').doc('IaRlvQrADIVN2lOxZytECU0fwGB3').get()
 //              .then(doc => doc.data());
@@ -24,7 +25,15 @@ const UserProfile = () => {
       <img src={user.userImage} alt={user.userName}/>
       <h6>{user.userName}</h6>
       <h6>{user.userSurname}</h6>
-      <h6>{user.aboutUser}</h6>
+      <Grid container
+        alignItems='center'>
+        <Grid container item xs={8}>
+          <h6>{user.aboutUser}</h6>
+        </Grid>
+        <Grid container item xs={3}>
+          <FormDialog/>
+        </Grid>
+      </Grid>
       <DropzoneDialogBlock/>
     </Grid>
   );
