201601a32ab2f30ca7b5fdd4967a53985c9781d3
asakoo2019 <asakoo2019@gmail.com>
2019-12-18 17:29:24 +0400

    sign

--- src/app/app.js
+++ src/app/app.js
@@ -13,17 +13,11 @@ const styles = {
 function App(props) {
   // const { classes } = props;
   return (
-<<<<<<< HEAD
     <Router>
       <Header />
       <NavBar />
       <Footer />
     </Router>
-=======
-    <div className={classes.main}>
-      <SignIn />
-    </div>
->>>>>>> c2d9706d663f506b62a90d0167e2b5a5afedb4ee
   );
 };
 
