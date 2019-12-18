740167799e9199864b5b67eb3580fb8d019bba64
Hovo91 <hovomnacakanyan91@gmail.com>
2019-12-18 16:54:17 +0400

    home

--- src/components/modal/modal.js
+++ src/components/modal/modal.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { DialogContent, DialogContentText, DialogActions, Dialog, TextareaAutosize, Button } from '@material-ui/core';
+import { withStyles } from "@material-ui/core/styles";
 
-export default function FormDialog() {
+const style = {
+  textArea: {
+    resize: 'none',
+    width: '100%',
+    outline: 'none'
+  }
+};
+
+function FormDialog(props) {
+  const { classes } = props;
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -20,34 +24,27 @@ export default function FormDialog() {
 
   return (
     <div>
-      <Button variant="outlined" color="primary" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+      <Button variant="outlined" color="primary" onClick={handleClickOpen}>+</Button>
+      <Dialog className={classes.formDialog} open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
         <DialogContent>
           <DialogContentText>
-            To subscribe to this website, please enter your email address here. We will send updates
-            occasionally.
+            Add summary to highlight your experience.
           </DialogContentText>
-          <TextField
+          <TextareaAutosize className={classes.textArea}
             autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-          />
+            fullWidth/>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose} color="primary">
             Cancel
           </Button>
           <Button onClick={handleClose} color="primary">
-            Subscribe
+            Save
           </Button>
         </DialogActions>
       </Dialog>
     </div>
   );
 }
+
+export default withStyles(style)(FormDialog);
