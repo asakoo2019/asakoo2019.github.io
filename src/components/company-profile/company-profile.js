a0055f3f61c8a46c5e918a88c695dcac22057ff9
Hovo91 <hovomnacakanyan91@gmail.com>
2019-12-18 17:38:22 +0400

    home

--- src/components/company-profile/company-profile.js
+++ src/components/company-profile/company-profile.js
@@ -1,18 +1,10 @@
 import React from 'react';
-<<<<<<< HEAD
-import { firestore } from '../firebase/db';
-import DropzoneDialogBlock from '../image-uploader-block';
 
-const user = firestore.collection('users').doc('IaRlvQrADIVN2lOxZytECU0fwGB3').get()
-             .then(doc => doc.data());
-console.log(user);
-=======
 // import { firestore } from '../firebase/db';
 import DropzoneDialogBlock from '../image-uploader-block';
 
 // const user = firestore.collection('users').doc('IaRlvQrADIVN2lOxZytECU0fwGB3').get()
 //              .then(doc => doc.data());
->>>>>>> home
 const CompanyProfile = () => {
   return (
     <div>
