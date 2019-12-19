aa9cd59cb94ae871558359aad6c1c652d62b6a88
Hovo91 <hovomnacakanyan91@gmail.com>
2019-12-19 12:07:58 +0400

    sign-in

--- src/app/app.js
+++ src/app/app.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router } from "react-router-dom";
-<<<<<<< HEAD
-=======
-
->>>>>>> 201601a32ab2f30ca7b5fdd4967a53985c9781d3
-
 
 import Header from '../components/header';
 import NavBar from '../components/header/nav-bar'
