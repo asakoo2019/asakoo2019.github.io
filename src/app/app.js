54562c5e8f2ff1a22d38fd55520d1bf5a355e1cf
karenmkrtumyan7 <karenmkrtumyan7@gmail.com>
2020-01-31 15:31:13 +0400

    lala

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { withStyles } from '@material-ui/core/styles';
 import { BrowserRouter as Router } from "react-router-dom";
 import store from '../components/react-redux/store';
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import Header from '../components/header';
 import NavBar from '../components/header/nav-bar'
 import Footer from '../components/footer';
+import { auth } from '../components/firebase/db';
 
-function App() {
+const styles = {
+  
+};
+
+function App(props) {
+const {dispatch} = props;
+  useEffect(() => {
+    let unmounted = false;
+    // const pathName = history.location.pathname;
+    // const LastSleshIndex = pathName.lastIndexOf('/');
+    // const searchId = pathName.slice(LastSleshIndex + 1);
+    auth.onAuthStateChanged((logedInCompany) => {
+      debugger;
+      // if (logedInCompany) {
+      //   dispatch({type: "SIGN-IN", payload: company});
+      //   // if (!searchId) {
+      //   //   setId(logedInCompany.uid);
+      //   //   // dispatch({type: "SIGN-OUT", payload: company});
+      //   // } else {
+      //   //   setId(searchId);
+      //   // }
+      // } else {
+      //   if(!unmounted){
+      //     setId(searchId);
+      //   };
+      // };
+    });
+    return () => {
+      unmounted = true;
+    };
+  }, []);
+
+  // const { classes } = props;
   return (
     <Provider store = {store}>
       <Router>
-        <Header />
-        <NavBar />
-        <Footer />
-      </Router>
+       <Header />
+       <NavBar />
+       <Footer />
+     </Router>
     </Provider>
   );
 };
-export default App;
+
+export default withStyles(styles)(App);
