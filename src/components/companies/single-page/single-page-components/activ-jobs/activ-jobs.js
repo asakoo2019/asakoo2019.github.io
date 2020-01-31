2e76c275b92d9c2171d577ca7d74a4c7c9dc204b
Hovsep Shirinyan <hovsep.shirinyan@yahoo.com>
2020-01-31 15:49:15 +0400

    Hovsep

--- src/components/companies/single-page/single-page-components/activ-jobs/activ-jobs.js
+++ src/components/companies/single-page/single-page-components/activ-jobs/activ-jobs.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 import { Button } from '@material-ui/core';
+import {useHistory} from 'react-router-dom';
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -13,17 +14,31 @@ const useStyles = makeStyles(theme => ({
 }));
 export default function ActiveJobs({ companyInfo }) {
     const classes = useStyles();
-    const jobs = companyInfo.companyJobs.slice(0, 3).map(item => {
+    const history = useHistory();
+    const [plus, setPlus] = useState(0)
+     const jobs = companyInfo.companyJobs.slice(plus, plus + 3).map(item => {
         return (
             <Grid
                 key={item.id}
             >
                 <div>{item.jobCategory}</div>
                 <div>here must be job datails</div>
-                <div><Button>View More</Button></div>
+                <div><Button
+                    onClick = {()=>history.push(`/jobs/${item.id}`) }
+                >View More</Button></div>
             </Grid>
         );
     })
+    function nextButton(){
+        
+        let x = (plus + 3<companyInfo.companyJobs.length)? plus + 1: plus;
+        setPlus( x )
+    }
+    function previousButton(){
+        
+        let x = (plus > 0)? plus - 1: plus;
+        setPlus( x )
+    }
     return (
         <Grid
             className={classes.root}
@@ -35,9 +50,13 @@ export default function ActiveJobs({ companyInfo }) {
                 justify="space-evenly"
                 alignItems="center"
             >
-                <Button>{'<'}</Button>
+                <Button 
+                    onClick = {previousButton}
+                >{'<'}</Button>
                     {jobs}
-                <Button>{'>'}</Button>
+                <Button
+                    onClick = {nextButton}
+                >{'>'}</Button>  
             </Grid>
         </Grid>
     );
