f41570ea9b29f7701d2ca2026efd4a0a33c5b3ee
Hovo91 <hovomnacakanyan91@gmail.com>
2020-02-05 10:42:21 +0400

    hovo

--- src/components/react-redux/search-reduser.js
+++ src/components/react-redux/search-reduser.js
@@ -2,6 +2,8 @@ export default function search(state = [], action) {
   switch (action.type) {
     case 'SEARCH':
       return action.payload;
+    case 'SEARCH-OFF':
+      return [];
     default:
       return state;
   };
